817c942479a88fb23d5781f9584b92a49cdbf5fe
kbwbe <kbwbe@gmx.de>
2018-08-11 15:56:36 +0200

    integrate DOF-calculation, work on rig.getCandidates()

--- a2plib.py
+++ a2plib.py
@@ -60,6 +60,14 @@ A2P_DEBUG_3         = 3
 
 A2P_DEBUG_LEVEL = A2P_DEBUG_NONE
 
+PARTIAL_SOLVE_STAGE1 = 1    #solve all rigid fully constrained to tempfixed rigid, enable only involved dep, then set them as tempfixed
+PARTIAL_SOLVE_STAGE2 = 2    #solve all rigid constrained only to tempfixed rigids, it doesn't matter if fully constrained or not. 
+                            #in case more than one tempfixed rigid
+PARTIAL_SOLVE_STAGE3 = 3    #repeat stage 1 and stage2 as there are rigids that match
+PARTIAL_SOLVE_STAGE4 = 4    #look for block of rigids, if a rigid is fully constrained to one rigid, solve them and create a superrigid (disabled at the moment)
+PARTIAL_SOLVE_STAGE5 = 5    #take all remaining rigid and dependencies not done and try to solve them all together
+PARTIAL_SOLVE_END = 6
+
 
 #------------------------------------------------------------------------------
 def setAutoSolve(enabled):
