d6cb145d4d329f9cdada08c828b9f73855c91ad1
kbwbe <kbwbe@gmx.de>
2018-10-27 14:32:01 +0200

    xml docreader, work in progress II

--- a2p_fcdocumentreader.py
+++ a2p_fcdocumentreader.py
@@ -48,7 +48,8 @@ class A2p_xmldoc_Property(object):
     '''
     BaseClass for xml-Properties
     '''
-    def __init__(self,name,_type):
+    def __init__(self,treeElement, name,_type):
+        self.treeElement = treeElement
         self.name = name
         self.type = _type
         print(self)
@@ -58,44 +59,57 @@ class A2p_xmldoc_Property(object):
     
 
 #------------------------------------------------------------------------------
+class A2p_xmldoc_PropertyString(A2p_xmldoc_Property):
+    
+    def getStringValue(self):
+        s = self.treeElement.find('String')
+        return s.attrib['value']
+    
+#------------------------------------------------------------------------------
+class A2p_xmldoc_PropertySheet(A2p_xmldoc_Property):
+    pass
+#------------------------------------------------------------------------------
 class A2p_xmldoc_Object(object):
     '''
     class prototype to store FC objects found in document.xml
     '''
-    def __init__(self,name,_type):
+    def __init__(self,name,_type, tree):
+        self.tree = tree
+        self.dataElement = None
         self.name = name
         self.type = _type
-        self.propertyList = []
+        self.propertyDict = {}
+        self.loadPropertyDict(self.tree)
+        self.label = self.propertyDict['Label'].getStringValue()
         print(self)
         
     def __str__(self):
-        return "ObjName: {}, Type: {}".format(self.name,self.type)
+        return "ObjName: {}, Label: {}, Type: {}".format(self.name, self.label, self.type)
     
-    def loadPropertyList(self,tree):
+    def loadPropertyDict(self,tree):
         for elem in tree.iterfind('ObjectData/Object'):
             if elem.attrib['name'] == self.name:
+                self.dataElement = elem
                 for e in elem.findall('Properties/Property'):
-                    p = A2p_xmldoc_Property(
-                        e.attrib['name'],
-                        e.attrib['type']
-                        )
-                    self.propertyList.append(p)
-                    
-    
-    def loadObjectData(self,tree):
-        raise NotImplementedError(
-            "xml-ojbect class {} doesn't allow loadObjectData, use inherited classes instead!".format(
-                self.__class__.__name__)
-            )
-        
+                    if e.attrib['type'] == 'App::PropertyString':
+                        p = A2p_xmldoc_PropertyString(
+                            e,
+                            e.attrib['name'],
+                            e.attrib['type']
+                            )
+                        self.propertyDict[e.attrib['name']] = p
+                    elif e.attrib['type'] == 'Spreadsheet::PropertySheet':
+                        p = A2p_xmldoc_PropertySheet(
+                            e,
+                            e.attrib['name'],
+                            e.attrib['type']
+                            )
+                        self.propertyDict[e.attrib['name']] = p
+                    else:
+                        pass # unsupported property type
 #------------------------------------------------------------------------------
 class A2p_xmldoc_SpreadSheet(A2p_xmldoc_Object):
-    def loadObjectData(self,tree):
-        '''
-        Extract Spreadsheed data from fcstd xml document
-        '''
-        self.loadPropertyList(tree)
-
+    pass
 #------------------------------------------------------------------------------
 class FCdocumentReader(object):
     '''
@@ -146,10 +160,10 @@ class FCdocumentReader(object):
             if elem.attrib['type'].startswith('Spreadsheet'): 
                 ob = A2p_xmldoc_SpreadSheet(
                         elem.attrib['name'],
-                        elem.attrib['type']
+                        elem.attrib['type'],
+                        self.tree
                         )
                 self.objects.append(ob)
-                ob.loadObjectData(self.tree)
             else:
                 pass # unhandled object types
 #------------------------------------------------------------------------------
