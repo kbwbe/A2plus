4c7c38f61b6557ce8453c7dc4d6607b05522d871
kbwbe <kbwbe@gmx.de>
2019-12-27 20:08:56 +0100

    fix missing partmover, fix import of links to a part-container

--- a2p_importpart.py
+++ a2p_importpart.py
@@ -961,6 +961,46 @@ FreeCADGui.addCommand('a2p_editImportedPart', a2p_EditPartCommand())
 
 
 #===============================================================================
+class PartMover:
+    def __init__(self, view, obj, deleteOnEscape):
+        self.obj = obj
+        self.initialPosition = self.obj.Placement.Base
+        self.view = view
+        self.deleteOnEscape = deleteOnEscape
+        self.callbackMove = self.view.addEventCallback("SoLocation2Event",self.moveMouse)
+        self.callbackClick = self.view.addEventCallback("SoMouseButtonEvent",self.clickMouse)
+        self.callbackKey = self.view.addEventCallback("SoKeyboardEvent",self.KeyboardEvent)
+        self.objectToDelete = None # object reference when pressing the escape key
+        
+    def moveMouse(self, info):
+        newPos = self.view.getPoint( *info['Position'] )
+        self.obj.Placement.Base = newPos
+        
+    def removeCallbacks(self):
+        self.view.removeEventCallback("SoLocation2Event",self.callbackMove)
+        self.view.removeEventCallback("SoMouseButtonEvent",self.callbackClick)
+        self.view.removeEventCallback("SoKeyboardEvent",self.callbackKey)
+        
+    def clickMouse(self, info):
+        if info['Button'] == 'BUTTON1' and info['State'] == 'DOWN':
+            #if not info['ShiftDown'] and not info['CtrlDown']: #struggles within Inventor Navigation
+            if not info['ShiftDown']:
+                self.removeCallbacks()
+                FreeCAD.activeDocument().recompute()
+            elif info['ShiftDown']:
+                self.obj = duplicateImportedPart(self.obj)
+                self.deleteOnEscape = True
+                
+    def KeyboardEvent(self, info):
+        if info['State'] == 'UP' and info['Key'] == 'ESCAPE':
+            self.removeCallbacks()
+            if not self.deleteOnEscape:
+                self.obj.Placement.Base = self.initialPosition
+            else:
+                self.objectToDelete = self.obj #This can be asked by a timer in a calling func...
+                #This causes a crash in FC0.19/Qt5/Py3             
+                #FreeCAD.activeDocument().removeObject(self.obj.Name)
+#===============================================================================
 toolTip = \
 '''
 Move the selected part.
