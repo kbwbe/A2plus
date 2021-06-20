b0cce5b73f2c658c5113e801c999316d3d363e12
kbwbe <kbwbe@gmx.de>
2021-06-20 15:27:05 +0200

    work on translations (WIP)

--- translations/A2plus.ts
+++ translations/A2plus.ts
@@ -1,6 +1,183 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<TS version="2.1">
+<context>
+    <name>Gui::Dialog::DlgSettingsA2Plus</name>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="20"/>
+        <source>A2plus settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="38"/>
+        <source>User interface settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="53"/>
+        <source>Adds a creation button for every constraint type to the toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="56"/>
+        <source>Show constraints in toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="72"/>
+        <source>Use native file manager of your OS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="107"/>
+        <source>Behavior when updating imported parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="122"/>
+        <source>All parts of the assembly will be opened in FreeCAD to be
+reconstructed using values from spreadsheets</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="126"/>
+        <source>Recalculate imported parts before updating them (experimental)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="139"/>
+        <source>Opens all subassemblies recursively
+to update them</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="143"/>
+        <source>Enable recursive update of imported parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="156"/>
+        <source>While importing parts to the assembly, the topological names
+are written into &quot;mux Info&quot; property. When the parts are
+later updated the properties &quot;Sub Elementx&quot; of the constraints
+will be updated according to the &quot;mux Info&quot; topology.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="162"/>
+        <source>Use experimental topological naming</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="175"/>
+        <source>Use color and transparency settings
+from imported parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="179"/>
+        <source>Inherit per face transparency from parts and subassemblies (experimental)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="192"/>
+        <source>Invisible datum/construction shapes will be hidden.
+Note: No constraints must be connected to
+datum/construction shapes in higher or other
+subassemblies. Otherwise you can break the assembly.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="198"/>
+        <source>All imported parts will directly be put together as union.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="201"/>
+        <source>Do not import invisible shapes (for expert users)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="214"/>
+        <source>Use solid union for importing parts and subassemblies (experimental)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="243"/>
+        <source>Storage of files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="258"/>
+        <source>Use relative paths for imported parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="274"/>
+        <source>Use absolute paths for imported parts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="287"/>
+        <source>Specify the project folder in the field below</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="290"/>
+        <source>All files are in this project folder:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="357"/>
+        <source>Default solver behavior</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="369"/>
+        <source>Solve automatically if a constraint property is changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="398"/>
+        <source>Default solving method</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="413"/>
+        <source>Solver begins with a fixed part and a part constrained to it.
+All other parts are not calculated. If a solution could be
+found, the next constrained part is added for the
+calculation and so on.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="419"/>
+        <source>Use solving of partial systems (recommended for static assemblies)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="435"/>
+        <source>Solver tries to move all parts at once
+in direction to a fixed part</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="439"/>
+        <source>Use &quot;magnetic&quot; solver, solving all parts at once (for dynamical assemblies)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="452"/>
+        <source>All parts will be fixed to the positions
+where they were created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../GuiA2p/Resources/ui/a2p_prefs.ui" line="456"/>
+        <source>Force fixed position to all imports</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>A2plus</name>
     <message>
@@ -29,29 +206,7 @@ Answer No:
 Only the parts within the
 recent assembly are collected.
 </source>
-        <translation>
-Erstelle eine Stücklistentabelle für diese Datei
-
-This function will read out
-the #PARTINFO# spreadsheet of
-all involved parts of the
-assembly and create a new
-spreadsheet containing the
-parts list.
-
-This button will open a dialog
-with the Question:
-- Iterate recursively over
-     all subassenblies?
-
-Answer Yes:
-All parts of all subassemblies are
-collected to the partlist
-
-Answer No:
-Only the parts within the
-recent assembly are collected.
-</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -59,7 +214,7 @@ recent assembly are collected.
     <message>
         <location filename="a2p_bom.py" line="263"/>
         <source>Create a spreadsheet with a parts list of this file</source>
-        <translation>Erstelle eine Stücklistentabelle für diese Datei</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -73,12 +228,12 @@ recent assembly are collected.
 <context>
     <name>A2plus_constraintDialog</name>
     <message>
-        <location filename="a2p_constraintDialog.py" line="1043"/>
+        <location filename="a2p_constraintDialog.py" line="1061"/>
         <source>Define constraints</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_constraintDialog.py" line="1094"/>
+        <location filename="a2p_constraintDialog.py" line="1112"/>
         <source>Edit selected constraint</source>
         <translation type="unfinished"></translation>
     </message>
@@ -180,137 +335,137 @@ recent assembly are collected.
 <context>
     <name>A2plus_importpart</name>
     <message>
-        <location filename="a2p_importpart.py" line="478"/>
+        <location filename="a2p_importpart.py" line="638"/>
         <source>Restore transparency to active document objects</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="516"/>
+        <location filename="a2p_importpart.py" line="676"/>
         <source>Add a part from an external file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="767"/>
+        <location filename="a2p_importpart.py" line="927"/>
         <source>Update parts imported into the assembly</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="870"/>
+        <location filename="a2p_importpart.py" line="1030"/>
         <source>Create duplicate of a part</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1002"/>
+        <location filename="a2p_importpart.py" line="1162"/>
         <source>Edit an imported part (open linked FCStd file)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1091"/>
+        <location filename="a2p_importpart.py" line="1253"/>
         <source>Move the selected part</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1208"/>
+        <location filename="a2p_importpart.py" line="1370"/>
         <source>Move the selected part under constraints</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1287"/>
+        <location filename="a2p_importpart.py" line="1449"/>
         <source>Delete all constraints of selected parts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1334"/>
+        <location filename="a2p_importpart.py" line="1496"/>
         <source>Highlight both constrained parts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1428"/>
+        <location filename="a2p_importpart.py" line="1590"/>
         <source>Show only selected elements or all if none is selected</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1460"/>
+        <location filename="a2p_importpart.py" line="1622"/>
         <source>Toggle transparency of assembly</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1461"/>
+        <location filename="a2p_importpart.py" line="1623"/>
         <source>Toggles transparency of assembly</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1493"/>
+        <location filename="a2p_importpart.py" line="1655"/>
         <source>Toggle auto solve</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1513"/>
+        <location filename="a2p_importpart.py" line="1675"/>
         <source>Toggle partial processing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1544"/>
+        <location filename="a2p_importpart.py" line="1706"/>
         <source>Repair the tree view if it is somehow damaged</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1575"/>
+        <location filename="a2p_importpart.py" line="1737"/>
         <source>Flip direction of last constraint</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1622"/>
+        <location filename="a2p_importpart.py" line="1784"/>
         <source>Generate HTML file with detailed constraining structure</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1625"/>
+        <location filename="a2p_importpart.py" line="1787"/>
         <source>Generates HTML file with detailed constraining structure</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1688"/>
+        <location filename="a2p_importpart.py" line="1850"/>
         <source>Show part labels in 3D view</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1689"/>
+        <location filename="a2p_importpart.py" line="1851"/>
         <source>Toggle showing part labels in 3D view</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1723"/>
+        <location filename="a2p_importpart.py" line="1885"/>
         <source>Print detailed DOF information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1724"/>
+        <location filename="a2p_importpart.py" line="1886"/>
         <source>Toggle printing detailed DOF information</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1759"/>
+        <location filename="a2p_importpart.py" line="1921"/>
         <source>Convert absolute paths of imported parts to relative ones</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1762"/>
+        <location filename="a2p_importpart.py" line="1924"/>
         <source>Converts absolute paths of imported parts to relative ones</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1794"/>
+        <location filename="a2p_importpart.py" line="1956"/>
         <source>Save and exit the active document</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="1852"/>
+        <location filename="a2p_importpart.py" line="2014"/>
         <source>Migrate proxies of imported parts</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_importpart.py" line="2000"/>
+        <location filename="a2p_importpart.py" line="2162"/>
         <source>Clean up solver debug output from 3D view</source>
         <translation type="unfinished"></translation>
     </message>
@@ -342,18 +497,18 @@ recent assembly are collected.
 <context>
     <name>A2plus_solversystem</name>
     <message>
-        <location filename="InitGui.py" line="53"/>
-        <source>An other assembly workbench for FreeCAD.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="a2p_solversystem.py" line="711"/>
+        <location filename="a2p_solversystem.py" line="735"/>
         <source>Solve constraints</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="a2p_solversystem.py" line="715"/>
+        <location filename="a2p_solversystem.py" line="739"/>
         <source>Solves constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="InitGui.py" line="53"/>
+        <source>An other assembly workbench for FreeCAD.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
