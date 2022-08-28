76de717071f7073abad271704d79dcce51cc502a
Stanislav <42420548+PushKK@users.noreply.github.com>
2022-08-29 01:01:15 +0300

    Add files via upload
    1. Add two renamed .py files,
    2. Add two .sh files,
    3. Add "read.me" file.

---
+++ translations/compile_qm.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#***************************************************************************
+#*                                                                         *
+#*   Copyright (c) 2019 kbwbe                                              *
+#*                                                                         *
+#*   Portions of code based on hamish's assembly 2                         *
+#*                                                                         *
+#*   This program is free software; you can redistribute it and/or modify  *
+#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
+#*   as published by the Free Software Foundation; either version 2 of     *
+#*   the License, or (at your option) any later version.                   *
+#*   for detail see the LICENCE text file.                                 *
+#*                                                                         *
+#*   This program is distributed in the hope that it will be useful,       *
+#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#*   GNU Library General Public License for more details.                  *
+#*                                                                         *
+#*   You should have received a copy of the GNU Library General Public     *
+#*   License along with this program; if not, write to the Free Software   *
+#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+#*   USA                                                                   *
+#*                                                                         *
+#***************************************************************************
+
+import os
+import glob
+
+#==============================================================================
+# Script for merging different translations of A2plus Workbench
+#
+# The script has to be started within the A2plus/translations Folder
+#==============================================================================
+
+for fn in glob.glob('*_*.ts'):
+    os.system(
+        '''
+        lrelease {}
+        '''.format(fn)
+        )
+
