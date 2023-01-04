0542b7c71f9e18f1bea96538a7f840f7959b9f80
Stanislav <42420548+PushKK@users.noreply.github.com>
2023-01-04 22:02:08 +0300

    Change for PEP8 and small fix
    1. Change for PEP8,
    2. Add one message for user,
    3. Change translate() context.

--- a2p_constraintServices.py
+++ a2p_constraintServices.py
@@ -1,46 +1,48 @@
-#***************************************************************************
-#*                                                                         *
-#*   Copyright (c) 2020 kbwbe                                              *
-#*                                                                         *
-#*   Portions of code based on hamish's assembly 2                         *
-#*                                                                         *
-#*   This program is free software; you can redistribute it and/or modify  *
-#*   it under the terms of the GNU Lesser General Public License (LGPL)    *
-#*   as published by the Free Software Foundation; either version 2 of     *
-#*   the License, or (at your option) any later version.                   *
-#*   for detail see the LICENCE text file.                                 *
-#*                                                                         *
-#*   This program is distributed in the hope that it will be useful,       *
-#*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-#*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-#*   GNU Library General Public License for more details.                  *
-#*                                                                         *
-#*   You should have received a copy of the GNU Library General Public     *
-#*   License along with this program; if not, write to the Free Software   *
-#*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
-#*   USA                                                                   *
-#*                                                                         *
-#***************************************************************************
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2020 kbwbe                                              *
+# *                                                                         *
+# *   Portions of code based on hamish's assembly 2                         *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+from PySide import QtGui
 
 import FreeCAD
 import FreeCADGui
-from PySide import QtGui
-from a2p_translateUtils import *
 import a2plib
 import a2p_constraints
 
-#==============================================================================
+translate = FreeCAD.Qt.translate
+
+
 def redAdjustConstraintDirections(doc):
     """
-    Recalculate value of property 'direction' and the sign of property 'offset' of all
-    a2p-constraints of a document, in order to reach a solvable state if
+    Recalculate value of property 'direction' and the sign of property 'offset'
+    of all a2p-constraints of a document, in order to reach a solvable state if
     possible, especially used after updating of imported parts.
     """
     result = []  # Added for Constraint Diagnostic function
     unknown_constraints = []
-    constraints = [ obj for obj in doc.Objects if 'ConstraintInfo' in obj.Content]
+    constraints = [obj for obj in doc.Objects if 'ConstraintInfo' in obj.Content]
     for c in constraints:
-        try: # process as much constraints as possible
+        try:  # process as much constraints as possible
             if c.Type == 'pointIdentity':
                 a2p_constraints.PointIdentityConstraint.recalculateMatingDirection(c)
             elif c.Type == 'pointOnLine':
@@ -72,24 +74,38 @@ def redAdjustConstraintDirections(doc):
             else:
                 unknown_constraints.append(c.Type)
         except:
-            print("Errors occurred during processing of {}".format(c.Label))
+        # Print - All not Ok, we have errors
+            print(translate(
+                            "A2plus",
+                            "Errors occurred during processing of {}").format(
+                            c.Label
+                            )
+                  )
             result.append(c.Name)   # Added for Constraint Diagnostic function
 
     if len(unknown_constraints) > 0:
-        print("redefineConstraintDirections(): Found unknown constraints: {}".format(
-            set(unknown_constraints)
-            )
-        )
+        # Print - All not Ok, we have problem
+        print(translate(
+                        "A2plus_Constraints",
+                        "redAdjustConstraintDirections(): Found unknown constraints: {}"
+                        ).format(
+                        set(unknown_constraints)
+                         )
+              )
+    else:
+        # Print - All Ok
+        print(translate("A2plus_Constraints", "A2plus: All constraints are recalculated."))
+
     return(result)                  # Added for Constraint Diagnostic function
-#==============================================================================
+
 
 class a2p_reAdjustConstraintDirectionsCommand:
     def Activated(self):
         flags = QtGui.QMessageBox.StandardButton.Yes | QtGui.QMessageBox.StandardButton.No
         response = QtGui.QMessageBox.information(
             QtGui.QApplication.activeWindow(),
-            translate("A2plus_constraintServices",'Recalculate direction of constraints'),
-            translate("A2plus_constraintServices",'Do you really want to recalculate the directions of all constraints?'),
+            translate("A2plus_Constraints", "Recalculate direction of constraints"),
+            translate("A2plus_Constraints", "Do you really want to recalculate the directions of all constraints?"),
             flags
             )
         if response == QtGui.QMessageBox.Yes:
@@ -99,14 +115,16 @@ class a2p_reAdjustConstraintDirectionsCommand:
             doc.commitTransaction()
 
     def IsActive(self):
-        if FreeCAD.activeDocument() is None: return False
+        if FreeCAD.activeDocument() is None:
+            return False
         return True
 
     def GetResources(self):
         return {
-            'Pixmap'  : a2plib.pathOfModule()+'/icons/a2p_ReAdjustConstraints.svg',
-            'MenuText': translate("A2plus_constraintServices", "Re-adjust directions of all constraints"),
-            'ToolTip' : translate("A2plus_constraintServices", "Re-adjust directions of all constraints to fit best")
+            'Pixmap': a2plib.pathOfModule()+'/icons/a2p_ReAdjustConstraints.svg',
+            'MenuText': translate("A2plus_Constraints", "Re-adjust directions of all constraints"),
+            'ToolTip': translate("A2plus_Constraints", "Re-adjust directions of all constraints to best fit")
             }
+
+
 FreeCADGui.addCommand('a2p_reAdjustConstraintDirectionsCommand', a2p_reAdjustConstraintDirectionsCommand())
-#==============================================================================
